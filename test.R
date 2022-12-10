a938f832cc73a75592c5ec8c1c3daedfe1468f62
scunn <shanecunn@hotmail.com>
2022-12-10 00:23:33 +0000

    test.R added for experimenting with r code, changes to csv. file on 22 and 14

---
+++ test.R
@@ -0,0 +1,19 @@
+# create mydata var
+mydata = read.csv("daie_ca3_data_14.csv")
+
+## select random rows from the data frame
+#sample_n(mydata, 3)
+
+## selecting random fraction of rows
+#sample_frac(mydata,0.1)
+
+## Remove Duplicate Rows based on all the variables (Complete Row)
+#x1 = distinct(mydata)
+
+## Remove Duplicate Rows based on a variable
+#x2 = distinct(mydata, test_group, .keep_all= TRUE)
+
+## Remove Duplicates Rows based on multiple variables
+#x2 = distinct(mydata, test_group, NA, .keep_all= TRUE)
+
+distinct_all(mydata)
